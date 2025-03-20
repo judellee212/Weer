51d895c097b6781351b342d0a2dd3553d7c9c3f6
judellee212 <judellee@yahoo.ca>
2025-03-20 10:55:28 +0100

    Injected more weather data

--- index.js
+++ index.js
@@ -3,18 +3,34 @@ function updateWeather(response) {
   let temperature = response.data.temperature.current;
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
-  let wind-speedElement = document.querySelector("#wind-speed");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time *1000);
 
 
 
-  cityElement = response.data.city;
+  cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-  wind-speedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  windspeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+  timeElement.innerHTML = formatDate(date);
 }
 
 
+function formatDate(date) {
+  
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+  let day = days[date.getDay()];
+  if (minutes < 10){
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours} ${minutes}`;
+
+}
 function search(city){
 let apikey = "a245cfb18ac0fc4e1161o64431840t19";
 let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
