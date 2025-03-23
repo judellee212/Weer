2da17984816c56090c0a44bd9f36a4b374d0792d
judellee212 <judellee@yahoo.ca>
2025-03-23 09:37:52 +0100

    Added forecast icons forEach in JS

--- index.js
+++ index.js
@@ -54,6 +54,12 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) { 
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+
+}
 function getForecast(city){
   let apiKey = "a245cfb18ac0fc4e1161o64431840t19";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -62,23 +68,28 @@ function getForecast(city){
 
 function displayForecast(response) {
   let forecastHtml = "";
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
-
-  days.forEach(function (day) {
-    forecastHtml =
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
       forecastHtml +
       `
       <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${day}</div>
-        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+        <div class="weather-forecast-icon">
+          <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+          </div>
         <div class="weather-forecast-temperatures">
           <div class="weather-forecast-temperature">
-            <strong>15º</strong>
+            <strong>${Math.round(day.temperature.maximum)}</strong>
+          </div>
+          <div class="weather-forecast-temperature">
+            <strong>${Math.round(day.temperature.minimum)}</strong>
           </div>
           <div class="weather-forecast-temperature">9º</div>
         </div>
       </div>
     `;
+    }
   });
 
 let forecastElement = document.querySelector("#forecast");
@@ -87,7 +98,5 @@ forecastElement.innerHTML = forecastHtml;
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-
-console.log(apiURL);
 
 searchCity("Paris");
